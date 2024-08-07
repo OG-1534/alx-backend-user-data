202b72775bc2b99d18f29c5d71ff6d6be49d5fba
OG-1534 <gershomochanda@gmail.com>
2024-08-07 22:30:45 +0300

    views.init.py

--- 0x01-Basic_authentication/api/v1/views/__init__.py
+++ 0x01-Basic_authentication/api/v1/views/__init__.py
@@ -1,8 +1,11 @@
+#!/usr/bin/env python3
+""" DocDocDocDocDocDoc
+"""
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__)
+app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views import index
-from api.v1.views import users
+from api.v1.views.index import *
+from api.v1.views.users import *
 
-__all__ = ['app_views']
+User.load_from_file()
