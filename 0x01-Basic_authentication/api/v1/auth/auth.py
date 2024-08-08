5f16704b943d66f313f145ccef8dc4369d9d908f
OG-1534 <gershomochanda@gmail.com>
2024-08-08 23:19:33 +0300

    lots of debugging

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -1,19 +1,39 @@
 #!/usr/bin/env python3
-
-from flask import request
+"""Module for the API authentication.
+"""
+import re
 from typing import List, TypeVar
+from flask import request
 
 
 class Auth:
+    """Authentication class.
+    """
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
-        """Checks if endpoint requires authentication """
-        return False
+        """Checks if an endpoint requires authentication.
+        """
+        if path is not None and excluded_paths is not None:
+            for exclusion_path in map(lambda x: x.strip(), excluded_paths):
+                pattern = ''
+                if exclusion_path[-1] == '*':
+                    pattern = '{}.*'.format(exclusion_path[0:-1])
+                elif exclusion_path[-1] == '/':
+                    pattern = '{}/*'.format(exclusion_path[0:-1])
+                else:
+                    pattern = '{}/*'.format(exclusion_path)
+                if re.match(pattern, path):
+                    return False
+        return True
 
     def authorization_header(self, request=None) -> str:
-        """Method handling authorization header """
+        """Retrieves authorization header from request.
+        """
+        if request is not None:
+            return request.headers.get('Authorization', None)
         return None
 
     def current_user(self, request=None) -> TypeVar('User'):
-        """Method to validate current user """
+        """Verifies current user from request.
+        """
         return None
