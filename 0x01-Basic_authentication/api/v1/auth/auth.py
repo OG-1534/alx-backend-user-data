39f03b9d40b57e048c49e57f74b9f2cd4c45dcd8
OG-1534 <gershomochanda@gmail.com>
2024-08-09 14:01:26 +0300

    Update the method def require_auth(self, path: str, excluded_paths: List[str]) -> bool: in Auth that, returns True if path is None, True if excluded_paths is None or empty, False if path is in excluded_paths, assume excluded_paths contains string path always ending by a /, method must be slash tolerant: path=/api/v1/status and path=/api/v1/status/ must be returned False if excluded_paths contains /api/v1/status/

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -12,7 +12,21 @@ class Auth:
 
     def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
         """ Checks if endpoint requires authentication """
-        return False
+
+        if path is None:
+            return True
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+
+        # Ensure path ends with a slash
+        if path[-1] != '/':
+            path += '/'
+
+        # Ensure path is in excluded_paths
+        if path in excluded_paths:
+            return False
+
+        return True
 
     def authorization_header(self, request=None) -> str:
         """ Retrieves authorization header from request """
