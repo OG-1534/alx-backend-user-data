91298a679f7fa420588aa995f23c3f0cb34ffaa2
OG-1534 <gershomochanda@gmail.com>
2024-08-09 15:34:31 +0300

    validate all requests to secure the API: biggest piece is the filtering of each request.

--- 0x01-Basic_authentication/api/v1/auth/auth.py
+++ 0x01-Basic_authentication/api/v1/auth/auth.py
@@ -30,7 +30,10 @@ class Auth:
 
     def authorization_header(self, request=None) -> str:
         """ Retrieves authorization header from request """
-        return None
+        if request is None:
+            return None
+        return request.headers.get('Authorization')
+
 
     def current_user(self, request=None) -> TypeVar('User'):
         """ Retrieves the current user from the request """
