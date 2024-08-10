486ce4b37ca3a46b878b55b6fa1a331ed0fee106
OG-1534 <gershomochanda@gmail.com>
2024-08-10 05:30:30 +0300

    Add the method def decode_base64_authorization_header(self, base64_authorization_header: str) -> str: in the class BasicAuth that returns the decoded value of a Base64 string base64_authorization_header

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -25,7 +25,6 @@ class BasicAuth(Auth):
         # Return the part after "Basic "
         return authorization_header[len("Basic "):]
 
-
     def decode_base64_authorization_header(
             self,
             base64_authorization_header: str) -> str:
