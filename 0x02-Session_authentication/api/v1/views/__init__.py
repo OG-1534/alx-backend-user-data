5e033e132205fd3c3fe2be7cb42153f65a9c4f9b
OG-1534 <gershomochanda@gmail.com>
2024-08-11 05:49:00 +0300

    updated __init__.py file

--- 0x02-Session_authentication/api/v1/views/__init__.py
+++ 0x02-Session_authentication/api/v1/views/__init__.py
@@ -2,10 +2,18 @@
 """ DocDocDocDocDocDoc
 """
 from flask import Blueprint
+from api.v1.views.index import *
+from api.v1.views.users import *
+from api.v1.views.session_auth import create_session_view
+from models.user import User
 
 app_views = Blueprint("app_views", __name__, url_prefix="/api/v1")
 
-from api.v1.views.index import *
-from api.v1.views.users import *
 
+# Register the new route
+app_views.add_url_rule('/auth_session/login',
+                       'create_session_view',
+                       create_session_view, methods=['POST'])
+
+# Load user data
 User.load_from_file()
