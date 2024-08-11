3d9eb2a03a5d235c978f683d392cc9ab532aab22
OG-1534 <gershomochanda@gmail.com>
2024-08-11 05:01:58 +0300

    Et moi et moi et moi! and empty session

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-
+"""
+Module for session authentication
+"""
+from base64 import b64decode
+import uuid
+from typing import Optional, TypeVar
 from api.v1.auth.auth import Auth
+from models.user import User
 
 
 class SessionAuth(Auth):
-    """Session authentication class that inherits from Auth"""
+    """
+    Session authentication class that inherits from Auth
+    """
     pass
