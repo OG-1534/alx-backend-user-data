679a05c3f365ab30a007fec382aefacb4ec7f6d9
OG-1534 <gershomochanda@gmail.com>
2024-08-11 05:39:41 +0300

    Create a new Flask view that handles all routes for the Session authentication.

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,13 +1,14 @@
 #!/usr/bin/env python3
 """
-Module for session authentication
+Module for session authentication views
 """
 from base64 import b64decode
 import uuid
 from typing import Optional, TypeVar
-from flask import request
+from flask import jsonify, request, make_response
 from api.v1.auth.auth import Auth
 from models.user import User
+from api.v1.auth.session_auth import SessionAuth
 
 
 class SessionAuth(Auth):
@@ -46,3 +47,36 @@ class SessionAuth(Auth):
             return None
 
         return User.get(user_id)
+
+    def create_session_view():
+    """ Handle POST /api/v1/auth_session/login """
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    # To validate email
+    if not email:
+        return jsonify({"error": "email missing"}), 400
+
+    # To validate password
+    if not password:
+        return jsonify({"error": "password missing"}), 400
+
+    # Retrieve User instance based on email
+    user = User.search(email=email)
+    if not user:
+        return jsonify({"error": "no user found for this email"}), 404
+
+    # Validate password
+    if not user.is_valid_password(password):
+        return jsonify({"error": "wrong password"}), 401
+
+    # Create Session ID
+    auth = SessionAuth()
+    session_id = auth.create_session(user.id)
+
+    # Create response
+    response = make_response(user.to_json())
+    cookie_name = request.environ.get('SESSION_NAME', '_my_session_id')
+    response.set_cookie(cookie_name, session_id)
+
+    return response
