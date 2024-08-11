d0f37e8bc4470000d375993ee326178d1fba6e17
OG-1534 <gershomochanda@gmail.com>
2024-08-11 05:24:36 +0300

    Add the URL path /api/v1/auth_session/login/ in the list of excluded paths of the method require_auth - this route doesn’t exist yet but it should be accessible outside authentication

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -55,18 +55,19 @@ def before_request():
     """
     excluded_paths = ['/api/v1/status/',
                       '/api/v1/unauthorized/',
-                      '/api/v1/forbidden/']
+                      '/api/v1/forbidden/',
+                      '/api/v1/auth_session/login/']
 
     if auth in None:
         return
     if not auth.require_auth(request.path, excluded_paths):
         return
+    if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
+        abort(401, description="Unauthorized")
 
     request.current_user = auth.current_user(request)
 
-    if auth.authorization_header(request) is None:
-        abort(401, description="Unauthorized")
-    if auth.current_user(request) is None:
+    if request.current_user is None:
         abort(403, description="Forbidden")
 
 
